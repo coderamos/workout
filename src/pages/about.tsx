66e61f8d29cc50d6173bd93e66b88e810fc29ee1
Diego Silva <diego.coderamos@gmail.com>
2021-07-10 17:02:41 -0300

    update about request

---
+++ src/pages/about.tsx
@@ -0,0 +1,25 @@
+import { GetStaticProps } from 'next';
+import { useRouter } from 'next/dist/client/router';
+
+import client from 'graphql/client';
+import { GET_CREATORS } from 'graphql/queries';
+
+import AboutTemplate, { AboutTemplateProps } from 'templates/About';
+
+export default function AboutPage({ creators }: AboutTemplateProps) {
+  const router = useRouter();
+  if (router.isFallback) return null;
+
+  return <AboutTemplate creators={creators} />;
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { creators } = await client.request(GET_CREATORS);
+  if (!creators) return { notFound: true };
+
+  return {
+    props: {
+      creators: creators
+    }
+  };
+};
--- src/templates/About/index.tsx
+++ src/templates/About/index.tsx
@@ -7,52 +7,61 @@ import WorkoutLogoText from 'assets/icons/WorkoutLogoText';
 
 import * as s from './styles';
 
-export type AboutTemplateProps = {
-  heading: string;
-  slug: string;
-  companyAvatar?: string;
-  companyName: string;
-  companyDescription: string;
-  developerAvatar?: string;
-  developerName: string;
-  developerDescription: string;
+export type CreatorProps = {
+  name: string;
+  avatar: string;
+  description: string;
 };
 
-const AboutTemplate = () => (
-  <s.Wrapper>
-    <LinkWrapper href="/">
-      <MapIcon size={32} aria-label="about" />
-    </LinkWrapper>
-    <s.HeaderWrapper>
-      <s.HeaderLogoWrapper>
-        <WorkoutLogoText />
-      </s.HeaderLogoWrapper>
-      <s.HeaderText>about</s.HeaderText>
-    </s.HeaderWrapper>
-    <Container>
-      <s.Content>
-        <s.BodyWrapper>
-          <s.ProfileCardWrapper>
-            <s.PictureWrapper src="https://avatars.githubusercontent.com/u/38539443?v=4" />
-            <s.ProfileName>workout</s.ProfileName>
-            <s.ProfileDescription>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum,
-              dolorum?
-            </s.ProfileDescription>
-          </s.ProfileCardWrapper>
-          <s.ProfileCardWrapper>
-            <s.PictureWrapper src="https://avatars.githubusercontent.com/u/38539443?v=4" />
-            <s.ProfileName>diego silva</s.ProfileName>
-            <s.ProfileDescription>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas
-              architecto odit dignissimos corrupti, consequuntur ut nemo nostrum
-              dicta accusamus est.
-            </s.ProfileDescription>
-          </s.ProfileCardWrapper>
-        </s.BodyWrapper>
-      </s.Content>
-    </Container>
-  </s.Wrapper>
-);
+export type AboutTemplateProps = {
+  creators: [
+    {
+      heading: string;
+      slug: string;
+      companyAvatar?: string;
+      companyName: string;
+      companyDescription: string;
+      developerAvatar?: string;
+      developerName: string;
+      developerDescription: string;
+    }
+  ];
+};
+
+const AboutTemplate = ({ creators }: AboutTemplateProps) => {
+  return (
+    <s.Wrapper>
+      <LinkWrapper href="/">
+        <MapIcon size={32} aria-label="about" />
+      </LinkWrapper>
+      <s.HeaderWrapper>
+        <s.HeaderLogoWrapper>
+          <WorkoutLogoText />
+        </s.HeaderLogoWrapper>
+        <s.HeaderText>{creators[0].heading}</s.HeaderText>
+      </s.HeaderWrapper>
+      <Container>
+        <s.Content>
+          <s.BodyWrapper>
+            <s.ProfileCardWrapper>
+              <s.PictureWrapper src="https://avatars.githubusercontent.com/u/38539443?v=4" />
+              <s.ProfileName>{creators[0].companyName}</s.ProfileName>
+              <s.ProfileDescription>
+                {creators[0].companyDescription}
+              </s.ProfileDescription>
+            </s.ProfileCardWrapper>
+            <s.ProfileCardWrapper>
+              <s.PictureWrapper src="https://avatars.githubusercontent.com/u/38539443?v=4" />
+              <s.ProfileName>{creators[0].developerName}</s.ProfileName>
+              <s.ProfileDescription>
+                {creators[0].developerDescription}
+              </s.ProfileDescription>
+            </s.ProfileCardWrapper>
+          </s.BodyWrapper>
+        </s.Content>
+      </Container>
+    </s.Wrapper>
+  );
+};
 
 export default AboutTemplate;
