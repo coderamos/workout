8611e9e6070e3a3fd22808d89cf15f6fbd6d70c9
Diego Silva <diego.coderamos@gmail.com>
2021-07-12 16:35:19 -0300

    update marker styles

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,6 +1,9 @@
 import { useRouter } from 'next/dist/client/router';
 
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet';
+import L from 'leaflet';
+
+import * as s from './styles';
 
 export type PlaceProps = {
   id: string;
@@ -34,33 +37,61 @@ const CustomTileLayer = () => {
   );
 };
 
+const markerIcon = new L.Icon({
+  iconUrl: 'img/marker.svg',
+  iconSize: [20, 28],
+  iconAnchor: [10, 28],
+  popupAnchor: [0, -40]
+});
+
 const Map = ({ places }: MapProps) => {
   const router = useRouter();
   return (
-    <MapContainer
-      center={[0, 0]}
-      zoom={3}
-      style={{ height: '100%', width: '100%' }}
-    >
-      <CustomTileLayer />
-      {places &&
-        places.map(({ id, name, slug, location }) => {
-          const { latitude, longitude } = location;
+    <s.Wrapper>
+      <MapContainer
+        center={[0, 0]}
+        zoom={3}
+        style={{ height: '100%', width: '100%' }}
+        minZoom={3}
+        maxBounds={[
+          [-180, 180],
+          [180, -180]
+        ]}
+      >
+        <MapConsumer>
+          {map => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth;
 
-          return (
-            <Marker
-              key={`place-${id}`}
-              position={[latitude, longitude]}
-              title={name}
-              eventHandlers={{
-                click: () => {
-                  router.push(`/place/${slug}`);
-                }
-              }}
-            />
-          );
-        })}
-    </MapContainer>
+            if (width < 768) {
+              map.setMinZoom(2);
+            }
+            return null;
+          }}
+        </MapConsumer>
+        <CustomTileLayer />
+        {places &&
+          places.map(({ id, name, slug, location }) => {
+            const { latitude, longitude } = location;
+
+            return (
+              <Marker
+                key={`place-${id}`}
+                position={[latitude, longitude]}
+                title={name}
+                icon={markerIcon}
+                eventHandlers={{
+                  click: () => {
+                    router.push(`/place/${slug}`);
+                  }
+                }}
+              />
+            );
+          })}
+      </MapContainer>
+    </s.Wrapper>
   );
 };
 
--- src/components/Map/styles.ts
+++ src/components/Map/styles.ts
@@ -1,5 +1,12 @@
 import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.div`
-  ${() => css``}
+  ${({ theme }) => css`
+    height: 100%;
+    width: 100%;
+
+    .leaflet-container {
+      background-color: ${theme.colors.black};
+    }
+  `}
 `;
